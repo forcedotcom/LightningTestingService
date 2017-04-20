d8c445aa05955a1d201ef3e40e26f4a49418fea4
cdengSFDC <cdeng@salesforce.com>
2017-04-20 13:42:34 -0700

    Inline comments update

--- lightning-component-tests/test/default/aura/BaseTestRunnerCmp/BaseTestRunnerCmpController.js
+++ lightning-component-tests/test/default/aura/BaseTestRunnerCmp/BaseTestRunnerCmpController.js
@@ -1,7 +1,7 @@
 ({
 	runTests : function(component, event, helper) {
         htmlReporter.initialize();
-        jasmine.getEnv().addReporter(new jasmineReporters.TapReporter());
+        jasmine.getEnv().addReporter(new jasmineReporters.Reporter());
     	jasmine.getEnv().execute();
 	}
 })
