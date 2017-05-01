729d25bffd760eafcac7ed3aae4be86be398aec6
Abhi <abhi.shah@salesforce.com>
2017-05-01 16:45:26 -0700

    Lightning Data Service tests

--- lightning-component-tests/main/default/aura/egLdsView/egLdsViewController.js
+++ lightning-component-tests/main/default/aura/egLdsView/egLdsViewController.js
@@ -1,67 +1,62 @@
 ({
-    doInit: function(component, event, helper) {
-        var e = component.get("e.createrec");//TODO
-        e.setParams({"entityApiName":"Contact", "recordTypeId":null, "defaultFieldValues":null, "skipCache":false});
-        e.fire();
+    doInit : function(cmp, event, helper){
     },
 
-    createRecordHandler: function(component, event, helper){
-        var rec = component.get("v.newContact");
-        var error = component.get("v.newContactError");
-        if(error || (rec === null)) {
-            console.log("Error initializing record template: " + error);
-        }
-        else {
-            console.log("Record template initialized: " + rec.sobjectType);
+    createRecord : function(cmp, event, helper) {
+        helper.createRecord(cmp);
+    },
+
+    reloadRecord : function(cmp, event, helper) {
+    	helper.reloadRecord(cmp);
+    },
+
+    saveRecord : function(cmp, event, helper) {
+    	helper.saveRecord(cmp);
+    },
+
+    deleteRecord : function(cmp, event, helper) {
+    	helper.deleteRecord(cmp);
+    },
+
+    handleRecordUpdate : function(cmp, event) {
+        //handle the recordUpdated event
+        var changeType = event.getParam("changeType");
+        if(changeType === "LOADED") {
+            // handle record loaded
+            cmp.set("v.logMessage", "Record has been loaded.");
+        } else if(changeType === "CHANGED") {
+            // handle record changed
+            cmp.set("v.logMessage", "Record has been changed.");
+        } else if(changeType === "REMOVED") {
+            // handle record removed
+            cmp.set("v.logMessage", "Record has been removed.");
+        } else if(changeType === "ERROR") {
+            // handle error while loading|saving|deleting record
+            cmp.set("v.logMessage", "There is some error while loading/updating record.");
+        } else {
+            // you should not get any other type than these 4 (as of Summer '17)
         }
     },
 
-    handleSaveContact: function(component, event, helper) {
-        if(helper.validateContactForm(component)) {
-            component.set("v.hasErrors", false);
-            component.set("v.newContact.AccountId", component.get("v.recordId"));
-            component.get("e.saverec").fire();//TODO
-        }
-        else {
-            // New contact form failed validation, show a message to review errors
-            component.set("v.hasErrors", true);
-        }
+    handleRecordTemplate : function(cmp, event, helper) {
+        //handle the recordUpdated event
     },
 
-    saveRecordHandler: function(component, event, helper){
-        var saveResult = event.getParam("arguments").saveResult; 
-        if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
-
-            // Success! Prepare a toast UI message
-            var resultsToast = $A.get("e.force:showToast");
-            resultsToast.setParams({
-                "title": "Contact Saved",
-                "message": "The new contact was created."
-            });
-
-            // Update the UI: close panel, show toast, refresh account page
-            $A.get("e.force:closeQuickAction").fire();
-            resultsToast.fire();
-
-            // Reload the view so components not using force:recordPreview
-            // are updated
-            $A.get("e.force:refreshView").fire();
-        }
-        else if (saveResult.state === "INCOMPLETE") {
-            console.log("User is offline, device doesn't support drafts.");
-        }
-        else if (saveResult.state === "ERROR") {
-            console.log('Problem saving contact, error: ' +
-                    JSON.stringify(saveResult.error));
-        }
-        else {
-            console.log('Unknown problem, state: ' + saveResult.state +
-                    ', error: ' + JSON.stringify(saveResult.error));
-        }
-
-    },
-
-    handleCancel: function(component, event, helper) {
-        $A.get("e.force:closeQuickAction").fire();
+    /**
+     * For testing purpose only (as you cannot get the getElement() of the component for other namespaces)
+     */
+    clickLightningBtn : function(cmp, event, helper) {
+    	var button = event.getParam("btnName");
+    	if(button === "create") {
+    		helper.createRecord(cmp);
+    	} else if(button === "reload") {
+    		helper.reloadRecord(cmp);
+    	} else if(button === "save") {
+    		helper.saveRecord(cmp);
+    	} else if(button === "delete") {
+    		helper.deleteRecord(cmp);
+    	} else {
+    		// do nothing
+    	}
     }
 })
--- lightning-component-tests/main/default/aura/egLdsView/egLdsViewHelper.js
+++ lightning-component-tests/main/default/aura/egLdsView/egLdsViewHelper.js
@@ -1,34 +1,52 @@
 ({
-    validateContactForm: function(component) {
-        var validContact = true;
+    createRecord : function(cmp) {
+        var recordDataCreate = cmp.find("recordDataCreate");
+        // call getNewRecord of force:recordData
+        recordDataCreate.getNewRecord("Account", null, true, function() {
+            // once we have  the record template for new record for given entity then update and save the record
+            var record = cmp.get('v.recordTemplate');
+            record.fields.Name.value = "ADSTestAccount" + (Math.round(Math.random()*10000) + 1);
+            record.fields.Industry.value = "Banking";
+            record.fields.Type.value = "Prospect";
+            recordDataCreate.saveRecord(function(saveResult) {
+                if (saveResult.state === 'SUCCESS') {
+                    cmp.set("v._recordId", saveResult.recordId);
+                    cmp.set('v.fetch', "true"); // this will load the Account record using force:recordData and renders it
+                    cmp.set("v.isCallbackCalled", true);
+                }
+            });
+        });
+    },
 
-        // First and Last Name are required
-        var firstNameField = component.find("contactFirstName");
-        if($A.util.isEmpty(firstNameField.get("v.value"))) {
-            validContact = false;
-            firstNameField.set("v.errors", [{message:"First name can't be blank"}]);
-        }
-        else {
-            firstNameField.set("v.errors", null);
-        }
-        var lastNameField = component.find("contactLastName");
-        if($A.util.isEmpty(lastNameField.get("v.value"))) {
-            validContact = false;
-            lastNameField.set("v.errors", [{message:"Last name can't be blank"}]);
-        }
-        else {
-            lastNameField.set("v.errors", null);
-        }
+    reloadRecord : function(cmp) {
+        cmp.find("recordDataCmp").reloadRecord(false, function(){
+            // verify record is reloaded. callback will be called after recordUpdated event is fired
+            cmp.set("v.logMessage", cmp.get("v.logMessage") + " After reloadRecord.");
+            cmp.set("v.isCallbackCalled", true);
+        });
+    },
 
-        // Verify we have an account to attach it to
-        var account = component.get("v.account");
-        if($A.util.isEmpty(account)) {
-            validContact = false;
-            console.log("Quick action context doesn't have a valid account.");
-        }
+    saveRecord : function(cmp) {
+        cmp.set("v.mode", "EDIT");
+        var recordDataCmp = cmp.find("recordDataCmp");
+        recordDataCmp.reloadRecord(false, function(){
+            var record = cmp.get("v.record");
+            record.fields.Name.value = "UpdatedRecordName";
+            recordDataCmp.saveRecord(function(){
+                // verify record is saved. callback will be called after recordUpdated event is fired
+                cmp.set("v.logMessage", cmp.get("v.logMessage") + " After saveRecord.");
+                cmp.set("v.mode", "VIEW");
+                cmp.set("v.isCallbackCalled", true);
+            });
+        });
+    },
 
-        // TODO: (Maybe) Validate email and phone number
-
-        return validContact;
+    deleteRecord : function(cmp) {
+        cmp.find("recordDataCmp").deleteRecord(function(){
+            // verify record is deleted. callback will be called after recordUpdated event is fired
+            cmp.set("v.logMessage", cmp.get("v.logMessage") + " After deleteRecord.");
+            cmp.set('v.fetch', "false");
+            cmp.set("v.isCallbackCalled", true);
+        });
     }
 })
